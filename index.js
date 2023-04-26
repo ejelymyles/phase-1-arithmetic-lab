4bc3418bef36711020e0cee48f6f81fc61f3ab48
Ej Myles <ejelymyles@gmail.com>
2023-04-25 18:21:30 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; 
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 67
+const num4 = 7
+const mod = 67 % 7;
+
+const max = Math.max(3, 6, 8, 11, 20, 17);
+
